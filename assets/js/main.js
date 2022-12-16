ef04fb3413a9b4e22c6bad89a483f1b041a8ce2f
h-t-a <htaythanaung@ucssittway.edu.mm>
2022-12-17 03:34:35 +0630

    First Step Error Solved

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,70 +7,36 @@ var p = inputSpeech.querySelector('p');
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 
-recognition.interimResults = true; // catch users speech while they are speaking
-recognition.continuous = true;
-// mic button event
-micBtn.addEventListener("click", function () {
-    if (micIcon.classList.contains('fa-microphone'))    //start speech recognition
-    {
-        recognition.start();
-    }
-    else //stop speech recognition
-    {
-        recognition.stop();
-    }
-});
 //answer and question
 var quest = [
-
     { question: 'who are you', answer: "I'm Webae" },
+    { question: 'thank you', answer: "You're welcome"},
+    { question: 'nice to meet you', answer: "nice to meet you too"},
     { question: "what's your name", answer: "My name is Webae" },
-    { question: 'can you speak', answer: "No, I can listen" },
+    { question: 'can you speak', answer: "No, I can listen to you" },
     { question: "what are you", answer: "My name is Webae. I have the ability to identify words spoken aloud and convert them into readable text using Web Speech API." },
     { question: "how are you", answer: "i'm fine" },
-    { question: 'what can you do for me', answer: "I can Listen for you, whatever you want to speak. I'll be here for you." },
-
+    { question: 'what can you do for me', answer: "I can Listen for you, whatever you want to speak. I'll be here for you." }
 ];
-// show on screen reply messages or answers
-var reply = document.createElement('p');
-reply.classList.add("reply");
-inputSpeech.appendChild(reply);
 
-//speech result
-recognition.onresult = (event) => {
-    var speech = " ";
+recognition.interimResults = true; // catch users speech while they are speaking
+recognition.continuous = true;
 
-     speech = Array.from(event.results)
-        .map(result => result[0])
-        .map(result => result.transcript)
-        .join('');
-    const res = Array.from(event.results).map(results => results.transcript)
-    console.log("full result", res);
-    p.innerText = speech;
-    reply.innerText = " ";
-
-    // console.log(result1)
-    var last = speech.length > 1;
-    for (let i = 0; i < quest.length; i++) {
-        if (speech.includes(quest[i].question)) {
-            reply.innerText = quest[i].answer;
-            console.log("array list" ,quest[i].question, quest[i].answer)
-
-        }
-        if (speech.includes("how many questions do you have")) {
-            var test = quest.map(q => q.question).join('\n');
-            console.log("testing",test);
-            reply.innerText = "I have a few questions for you:\n"+ test;
-        }
-        console.log("Result :", last.valueOf())    
+// mic button event
+micBtn.addEventListener("click", function () {
+    if (micIcon.classList.contains('fa-microphone'))    
+    {   
+        //start speech recognition
+        recognition.start();
+    }
+    else 
+    {
+        //stop speech recognition
+        recognition.stop();
 
     }
-}
-// function check(param){
-//     quest.forEach(element){
-//         param = element.question
-//     }
-// }
+});
+
 recognition.onstart = () => {
     micIcon.classList.remove("fa-microphone");
     micIcon.classList.add("fa-microphone-slash");
@@ -81,4 +47,33 @@ recognition.onend = () => {
     reply.innerText = " ";
     micIcon.classList.remove("fa-microphone-slash");
     micIcon.classList.add("fa-microphone");
+   
 }
+
+
+var reply = document.createElement('p');
+reply.classList.add("reply");
+inputSpeech.appendChild(reply);
+
+//speech result
+recognition.onresult = (event) => {
+   reply.innerText = " ";
+
+    var speech =  Array.from(event.results)
+       .map(result => result[0])
+       .map(result => result.transcript)
+       .join('');
+       
+   p.innerText = speech;
+
+   for (let i = 0; i < event.results.length; i++) {
+        for (let j = 0; j < quest.length; j++) {
+            var test = event.results[i][0].transcript;
+            if(test.includes(quest[j].question)){
+                reply.innerText = quest[j].answer;
+            }
+        }
+   }
+   
+}
+
